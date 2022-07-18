658d6f97cc7b776ac02b1cab0cad9e54ffdfc4e3
Samantha Wu <samanthayywu@gmail.com>
2022-07-18 14:47:55 +0100

    form completed

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Input = () => {
   const [topic, setTopic] = useState("");
+  const [deadline, setDeadline] = useState("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,6 +19,17 @@ const Input = () => {
           value={topic}
           onChange={(e) => setTopic(e.target.value)}
         />
+        <label>Done by:</label>
+        <select value={deadline} onChange={(e) => setDeadline(e.target.value)}>
+          <option value="1-week">1 week</option>
+          <option value="2-week">2 weeks</option>
+          <option value="3-week">3 weeks</option>
+          <option value="4-week">4 weeks</option>
+          <option value="2-months">2 months</option>
+          <option value="3-4-months">3-4 months</option>
+          <option value="6-months">6 months</option>
+          <option value="ongoing">Ongoing</option>
+        </select>
       </form>
     </div>
   );
